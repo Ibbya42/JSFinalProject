3b00ba268dba16c58294df72f2ac2e16046e3e66
ibbya42 <ibbyali4214@gmail.com>
2024-02-13 15:28:56 +1300

    13/02 Final Push

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // API Key for Movie List - http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9
 
+async function main(){
+    const movies = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9")
+    const movieData = await movies.json();
+    const moviesList = document.querySelector('.movieList')
+    moviesList.innerHTML = movieData.map((movies) => moviesHTML(movies)).join("");
+}
+
+
