fb73f6eca81b53683bf638f31927a001d56c280b
ibbya42 <ibbyali4214@gmail.com>
2024-02-14 16:02:29 +1300

    14/02 Push

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 async function main(){
     const movies = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9")
     const movieData = await movies.json();
-    const moviesList = document.querySelector('.movieList')
-    moviesList.innerHTML = movieData.map((movies) => moviesHTML(movies)).join("");
+    const moviesList = document.querySelector('.movie')
+    moviesList.innerHTML = movieData.map((movie) => moviesHTML(movie)).join("");
+}
+
+function moviesHTML(movie){
+    return `
+    <div class="movie">
+    <div class="movie__card">
+    <p><b>Title:<b>${movie.Title}</p>
+    <figure>
+    <img src="/assets/Nav__img.png" class="movie__img">
+    </figure>
+    <p>Year: </p>
+    </div>
+    </div>`;
 }
 
 
