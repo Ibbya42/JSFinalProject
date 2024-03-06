df15d7d420d20b4e35ed7313cb6dfbf0445b5dcc
ibbya42 <ibbyali4214@gmail.com>
2024-03-06 15:05:54 +1300

    06/03 Api Update

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 // API Key for Movie List - http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9
 
-async function main(){
-    const movies = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9")
-    const movieData = await movies.json();
-    const moviesList = document.querySelector('.movie')
-    moviesList.innerHTML = movieData.map((movie) => moviesHTML(movie)).join("");
-}
+// async function main(){
+//     const movies = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=6f2ac8c9")
+//     const movieData = await movies.json();
+//     const moviesList = document.querySelector('.movie')
+//     moviesList.innerHTML = movieData.map((movie) => moviesHTML(movie)).join("");
+// }
 
-function moviesHTML(movie){
-    return `
-    <div class="movie">
+// main();
+
+// function moviesHTML(movie){
+//     return `
+//     <div class="movie">
+//     <div class="movie__card">
+//     <p><b>Title:<b>${movie.Title}</p>
+//     <figure>
+//     <img src="/assets/Nav__img.png" class="movie__img">
+//     </figure>
+//     <p>Year: </p>
+//     </div>
+//     </div>`;
+// }
+
+async function main(){
+    const movies = await fetch("https://jsonplaceholder.typicode.com/users");
+    const moviesData = await movies.json();
+    const movieListEL = document.querySelector('.Movie__List');
+    console.log(moviesData)
+    movieListEL.innerHTML = moviesData.
+    map(
+        (movie) =>
+    `<div class="movie">
     <div class="movie__card">
-    <p><b>Title:<b>${movie.Title}</p>
+    <p>Name: ${movie.name}</p>
     <figure>
     <img src="/assets/Nav__img.png" class="movie__img">
     </figure>
-    <p>Year: </p>
+    <p>Year: ${movie.phone} </p>
     </div>
-    </div>`;
+    </div>`
+        ).join("")
+ 
 }
 
-
+main();
